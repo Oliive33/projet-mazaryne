c5765982c5afce2b79ac01c488a4578b29ae13c4
olivier villevieille <oliviervillevieille@hotmail.fr>
2022-02-17 18:56:59 +0100

    modif page produit

--- js/products.js
+++ js/products.js
@@ -39,3 +39,51 @@ rangeInput.forEach((input) => {
     }
   });
 });
+// tablet & phone categories
+let sideMenu = document.querySelector("#side-menu");
+let sideBar = document.querySelector(".side-bar");
+
+sideMenu.onclick = () => {
+  sideMenu.classList.toggle("fa-times");
+  sideBar.classList.toggle("active");
+};
+
+let categoryBtn = document.querySelectorAll(".category .btn-categorie");
+
+categoryBtn.forEach((btn) => {
+  btn.onclick = () => {
+    categoryBtn.forEach((remove) => remove.classList.remove("active"));
+    btn.classList.add("active");
+  };
+});
+
+let typeBtn = document.querySelectorAll(".type .btn-categorie");
+
+typeBtn.forEach((btn) => {
+  btn.onclick = () => {
+    typeBtn.forEach((remove) => remove.classList.remove("active"));
+    btn.classList.add("active");
+  };
+});
+// ==================Price range===================//
+// const rangeInput = document.querySelectorAll(".range-input input"),
+//   priceInput = document.querySelectorAll(".price-input input"),
+//   range = document.querySelector(".slider .progress");
+// let priceGap = 1;
+
+// priceInput.forEach((input) => {
+//   input.addEventListener("input", (e) => {
+//     let minPrice = parseInt(priceInput[0].value),
+//       maxPrice = parseInt(priceInput[1].value);
+
+//     if (maxPrice - minPrice >= priceGap && maxPrice <= rangeInput[1].max) {
+//       if (e.target.className === "input-min") {
+//         rangeInput[0].value = minPrice;
+//         range.style.left = (minPrice / rangeInput[0].max) * 100 + "%";
+//       } else {
+//         rangeInput[1].value = maxPrice;
+//         range.style.right = 100 - (maxPrice / rangeInput[1].max) * 100 + "%";
+//       }
+//     }
+//   });
+// });
