348b9574adb0fbbb72acc477ab26d29710567b3a
olivier villevieille <oliviervillevieille@hotmail.fr>
2022-03-06 06:38:19 +0100

    ajout et modif js

--- js/main.js
+++ js/main.js
@@ -79,6 +79,25 @@ formBtn.addEventListener("click", () => {
 formClose.addEventListener("click", () => {
   loginForm.classList.remove("active");
 });
+const sign_in_btn = document.querySelector("#sign-in-btn");
+const sign_up_btn = document.querySelector("#sign-up-btn");
+const container = document.querySelector(".container-form");
+const sign_in_btn2 = document.querySelector("#sign-in-btn2");
+const sign_up_btn2 = document.querySelector("#sign-up-btn2");
+
+sign_up_btn.addEventListener("click", () => {
+  container.classList.add("sign-up-mode");
+});
+sign_in_btn.addEventListener("click", () => {
+  container.classList.remove("sign-up-mode");
+});
+sign_up_btn2.addEventListener("click", () => {
+  container.classList.add("sign-up-mode2");
+});
+sign_in_btn2.addEventListener("click", () => {
+  container.classList.remove("sign-up-mode2");
+});
+
 //Apparition Pop up lang
 let LangBtn = document.querySelector("#langues-btn");
 let loginLang = document.querySelector(".login-lang-container");
